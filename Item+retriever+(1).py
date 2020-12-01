61ecd676376a115ca9f1c6e74a23eba8d80124aa
Mahleat17 <72724400+Mahleat17@users.noreply.github.com>
2020-11-30 22:36:27 -0600

    Add files via upload

---
+++ Item+retriever+(1).py
@@ -0,0 +1,17 @@
+#Mahleat Kidanemariam
+#11/30/2020
+# This program retrieves the player.
+
+import main_character as mainc
+import Item
+
+
+
+player = mainc.main_character
+
+Matti = Item.Item("Main Player/cake baker")
+
+print(Matti.name)
+
+
+  
---
+++ Item+retriever+(2).py
@@ -0,0 +1,17 @@
+#Mahleat Kidanemariam
+#11/30/2020
+# This program contains the proerties of a main character 
+
+class Item:
+    name = ""
+    height = ""
+    weight = ""
+    skin_tone = ""
+    gender = ""
+    age = ""
+
+    def __init__(self, new_name):
+         self.name = new_name
+
+
+          
